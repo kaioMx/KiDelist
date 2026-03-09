99a2de3fcb2106380e1495cf55c5b2ba84c700af
kaiomaximoaraujo@gmail.com <kaiomaximoaraujo@gmail.com>
2026-03-09 15:35:31 -0300

    Tela tarefa melhorada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.kidelist"
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 29
+        targetSdk = 29
         versionCode = 1
         versionName = "1.0"
 
